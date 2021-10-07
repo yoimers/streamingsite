f354e1b09b3e576ed9310ff629f3e3982376746f
yoimers <hiseica2536@gmail.com>
2021-10-08 00:33:17 +0900

    aa

--- src/lib/UploadImage.ts
+++ src/lib/UploadImage.ts
@@ -1,26 +1,29 @@
 import { ref, uploadBytes } from "firebase/storage";
 import { ImageStateType } from "../../pages/broadcast";
 import { storage } from "./firebase";
+import imageCompression from "browser-image-compression";
 
 export const UploadImage = async (
   //画像をアップロード
   bucket: string,
   imageState: ImageStateType | null | undefined,
-  filename: string
+  filename: string,
+  maxSizeMB: number,
+  maxWidthOrHeight: number,
+  initialQuality: number
 ) => {
-  if (
-    imageState &&
-    imageState.binaryStr &&
-    typeof imageState.binaryStr !== "string"
-  ) {
+  if (imageState) {
+    const options = {
+      maxSizeMB,
+      maxWidthOrHeight,
+      initialQuality,
+    };
     //imageStateがnullなら何もしない
-    await uploadBytes(
-      ref(storage, `${bucket}/${filename}`),
-      imageState.binaryStr,
-      {
-        cacheControl: "public,max-age=600",
-        contentType: imageState.file.type,
-      }
-    );
+
+    const compressedFile = await imageCompression(imageState.file, options);
+    await uploadBytes(ref(storage, `${bucket}/${filename}`), compressedFile, {
+      cacheControl: "public,max-age=600",
+      contentType: imageState.file.type,
+    });
   }
 };
