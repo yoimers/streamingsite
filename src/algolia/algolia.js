461d25b8e2c58a74ceffad9ea7a9816d409b548c
yoimers <hiseica2536@gmail.com>
2021-10-15 04:11:59 +0900

    aa

--- src/algolia/algolia.js
+++ src/algolia/algolia.js
@@ -5,12 +5,3 @@ export const client = algoliasearch(
   "45d55ae47cebf4624639f56cff4bd128"
 );
 export const index = client.initIndex("your_index_name");
-
-const publicKey = client.generateSecuredApiKey(
-  "98f6672819d09a49b9b011ffec8f7a4f",
-  {
-    filters: "_tags:user_firebase",
-  }
-);
-
-console.log(publicKey);
